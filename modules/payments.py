25d7600d6a2a2851ba10bcc446b52822fa42cb5f
DavidMurimiMuriithi <135316487+DavidMurimiMuriithi@users.noreply.github.com>
2025-03-28 14:26:29 +0300

    Update payments.py

--- modules/payments.py
+++ modules/payments.py
@@ -8,7 +8,7 @@ def get_db_connection():
     connection = mysql.connector.connect(
    host='localhost',
         user='root',      # Replace with your MySQL username
-        password='Mwea1234',  # Replace with your MySQL password
+        password='',  # Replace with your MySQL password
         database='mwea_logistics' 
     )
     return connection
